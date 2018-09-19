75e16e472318f66435b91ec83cc5ef2a1e3a74c3
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-19 14:54:09 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,7 +5,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = CNd8zSc3spMrzT4dcW91l442WGyyKx10GtBXMipCCo59ystfdnCk+hCnT107i9SPVMtG0V7qQA9f2sJl/j26f4mh3hqR/MFLirUKV2yW/nYfbRFvzgPEsNOXgZ1KDunmJP9f0yKyIsvico4v+W4tEgdB04t89/1O/w1cDnyilFU
 $channelSecret = '3312897e4d69e4600c1890f92198d9fa';
-$idPush = 'U1c51e0bc876c27e71984f89b66a5363e'
+$idPush = 'SCMBOT01'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
