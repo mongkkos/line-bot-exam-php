34576dba69390c984131be6e1a1dd332c69e715b
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-19 16:10:39 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'Mdi5zLvkh05KApRX+nJAKjH5+8vQ0vkbldh4w3CUmYO7I1OwYI9/6R2TjL1XcGJpVMtG0V7qQA9f2sJl/j26f4mh3hqR/MFLirUKV2yW/nbh4y4G2h+AyIXgUeNPp6/V0oJhoNOGG7m4RKahgFCHigdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '3312897e4d69e4600c1890f92198d9fa';
+$channelSecret = 'dd2eb3a5ea419772cf875aaf60139e5f';
 
 $pushID = 'U1c51e0bc876c27e71984f89b66a5363e';
 
