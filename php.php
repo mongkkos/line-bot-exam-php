ccd20b5bd96ef390158c44e6437ed458c139fcb3
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-20 16:03:02 +0700

    Update php.php

--- php.php
+++ php.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 require "vendor/autoload.php";
 
 $access_token = 'P+XQxskXjlglPW1w7JPk28AssQdJfHmv05voI1Lg+DiTMNZA3kMKHA/nRjzPhjs/TMbsRtoavduuKbKXJMtFRtKDD/eu64PFjcoLJsbGnSFvCre6mNsH8RyX1l9sjRvDqZ7rAMw1DOk4XiUH39ugsQdB04t89/1O/w1cDnyilFU=';
@@ -13,6 +15,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('เฌอปรางคะ สวัสดีโอตะ');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
+
 
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
