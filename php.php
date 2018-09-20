1ead217d5b53df02557f4736f20dd6196d25cfd6
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-20 15:02:22 +0700

    Update php.php

--- php.php
+++ php.php
@@ -13,11 +13,6 @@ $arrPostData['to'] = "Ub02584573617660964d9d0ccf0469706";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "เฌอปรางคะ สวัสดีโอตะ";
 
-$arrPostData = array();
-$arrPostData['to'] = "Ub02584573617660964d9d0ccf0469706";
-$arrPostData['messages'][0]['type'] = "สวัสดี";
-$arrPostData['messages'][0]['text'] = "ว่าไงคะ";
-
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
