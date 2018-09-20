2a532b5f091878669c0faff9f803fe57e46f353e
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-20 14:15:43 +0700

    Update php.php

--- php.php
+++ php.php
@@ -13,7 +13,11 @@ $arrPostData['to'] = "Ub02584573617660964d9d0ccf0469706";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "เฌอปรางคะ สวัสดีโอตะ";
 
-
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "เฌอปรางคะ";
 
 
 $ch = curl_init();
