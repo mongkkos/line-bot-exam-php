ce36f63b122bdeb4206f46098aa77910a1bd95a8
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-20 15:57:01 +0700

    Update php.php

--- php.php
+++ php.php
@@ -1,11 +1,17 @@
 <?php
+
+require "vendor/autoload.php";
+
 $access_token = 'P+XQxskXjlglPW1w7JPk28AssQdJfHmv05voI1Lg+DiTMNZA3kMKHA/nRjzPhjs/TMbsRtoavduuKbKXJMtFRtKDD/eu64PFjcoLJsbGnSFvCre6mNsH8RyX1l9sjRvDqZ7rAMw1DOk4XiUH39ugsQdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/verify';
-$headers = array('Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result;
+$channelSecret = 'ad20a90033746dbfdb5aab77f32e7e9a';
+$pushID = 'Ub02584573617660964d9d0ccf0469706';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+
+textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('เฌอปรางคะ สวัสดีโอตะ');
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
