fe744785e3065de9a8d3f2b9563c9f8dea97132e
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-20 09:57:50 +0700

    Update php.php

--- php.php
+++ php.php
@@ -1,19 +1,7 @@
-<?php
-
-
-require "vendor/autoload.php";
-
-$access_token = 'P+XQxskXjlglPW1w7JPk28AssQdJfHmv05voI1Lg+DiTMNZA3kMKHA/nRjzPhjs/TMbsRtoavduuKbKXJMtFRtKDD/eu64PFjcoLJsbGnSFvCre6mNsH8RyX1l9sjRvDqZ7rAMw1DOk4XiUH39ugsQdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'ad20a90033746dbfdb5aab77f32e7e9a';
-$pushID = 'Ub02584573617660964d9d0ccf0469706';
-
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('เฌอปรางคะ สวัสดีโอตะ');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
-
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+{
+    "_id": {
+        "$oid": "Ub02584573617660964d9d0ccf0469706"
+    },
+    "question": "สวัสดี",
+    "answer": "ก๊าบผม"
+}
