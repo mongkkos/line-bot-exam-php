5449d4a59ff03d5f16b0460435751e6b143ac926
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-20 16:13:29 +0700

    Update php.php

--- php.php
+++ php.php
@@ -12,6 +12,14 @@ $pushID = 'Ub02584573617660964d9d0ccf0469706';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
+if($message == "สวัสดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+
+
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('เฌอปรางคะ สวัสดีโอตะ');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
