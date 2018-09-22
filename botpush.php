0bcbed828255383a8b77f33f6be808a6ca7593e9
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-22 16:08:05 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -16,8 +16,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("เฌอปรางคะ สวัสดีโอตะ");
 $response = $bot->pushMessage($pushID, $textMessageBuilder);continue;
 
-//$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
-$bot->replyMessage($event->getReplyToken(), $outputText);
+
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
