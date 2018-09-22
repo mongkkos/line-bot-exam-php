31a1724a1a698a05fe375707b56d3690c57cc69b
mongkkos <43336702+mongkkos@users.noreply.github.com>
2018-09-22 14:16:35 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'Ub02584573617660964d9d0ccf0469706';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('งง');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("งง");
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 
